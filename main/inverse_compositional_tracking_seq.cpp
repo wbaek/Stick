e50d3d60342debf70f890071d4f3b3c4fe9602c8
wbaek <wbaek.personal@gmail.com>
2015-03-31 22:41:15 +0900

    restart inverse compositional template tracking

--- main/inverse_compositional_tracking_seq.cpp
+++ main/inverse_compositional_tracking_seq.cpp
@@ -9,42 +9,16 @@
 #include <utils/others.hpp>
 #include <opencv2/opencv.hpp>
 
-#include <tracking/affine.hpp>
-#include <tracking/homography.hpp>
-#include <tracking/inverse_compositional.hpp>
-
-typedef Tracking::Affine MODEL;
-
-cv::Point transform(const cv::Mat& pose, int x, int y) {
-    MODEL model;
-    model.setModel(pose);
-    return model.transform(cv::Point(x, y));
-}
-
-void drawAffine(cv::Mat& image, cv::Mat& affine, const cv::Size kTemplateSize, const cv::Scalar& color=cv::Scalar(0, 255, 0), const int thickness=1) {
-    std::vector<cv::Point> pointList;
-    pointList.push_back( transform(affine, -kTemplateSize.width/2, -kTemplateSize.height/2) );
-    pointList.push_back( transform(affine, -kTemplateSize.width/2, +kTemplateSize.height/2) );
-    pointList.push_back( transform(affine, +kTemplateSize.width/2, +kTemplateSize.height/2) );
-    pointList.push_back( transform(affine, +kTemplateSize.width/2, -kTemplateSize.height/2) );
-    pointList.push_back( transform(affine, -kTemplateSize.width/2, -kTemplateSize.height/2) );
-
-    cv::Point delta(image.size().width/2, image.size().height/2);
-    for(int i=0; i<pointList.size()-1; i++) {
-        cv::line( image,  pointList[i]+delta, pointList[i+1]+delta, color, thickness);
-    }
-}
-
 void help(char* execute) {
-    std::cerr << "usage: " << execute << " [-h] -p DATA_PATH [-t TEMPLATE_SIZE] [-e EPSILON_VALUE] [-k ITERATION] [-v]" << std::endl;
+    std::cerr << "usage: " << execute << " [-h] -p DATA_PATH [-t TEMPLATE_SIZE] [-g GAUSSIAN_KERNAL_SIZE] [-e EPSILON_VALUE] [-k ITERATION] [-v]" << std::endl;
     std::cerr << "" << std::endl;
-    std::cerr << "\t-h, --help                     show this help message and exit" << std::endl;
-    std::cerr << "\t-p, --path      DATA_PATH      set DATA_PATH" << std::endl;
-    std::cerr << "\t-t, --template  SIZE           set TEMPLATE_SIZE (default:180)" << std::endl;
-    std::cerr << "\t-g, --gaussian  KERNAL_SIZE    set GAUSSIAN_KERNAL_SIZE (default:9)" << std::endl;
-    std::cerr << "\t-e, --epsilon   EPSILON_VALUE  set EPSILON_VALUE (default:0.05)" << std::endl;
-    std::cerr << "\t-k, --iteration ITERATION      set max ITERATION per update (default:100)" << std::endl;
-    std::cerr << "\t-v, --verbose                  verbose" << std::endl;
+    std::cerr << "\t-h, --help                           show this help message and exit" << std::endl;
+    std::cerr << "\t-p, --path      DATA_PATH            set DATA_PATH" << std::endl;
+    std::cerr << "\t-t, --template  SIZE                 set TEMPLATE_SIZE (default:180)" << std::endl;
+    std::cerr << "\t-g, --gaussian  GAUSSIAN_KERNAL_SIZE set GAUSSIAN_KERNAL_SIZE (default:9)" << std::endl;
+    std::cerr << "\t-e, --epsilon   EPSILON_VALUE        set EPSILON_VALUE (default:0.05)" << std::endl;
+    std::cerr << "\t-k, --iteration ITERATION            set max ITERATION per update (default:100)" << std::endl;
+    std::cerr << "\t-v, --verbose                        verbose" << std::endl;
     exit(-1);
 }
 
@@ -100,39 +74,18 @@ int main(int argc, char* argv[]) {
     std::vector<std::string> filelist;
     instant::Utils::Filesystem::GetFileNames(dataPath, filelist);
 
-    Tracking::InverseCompositional<MODEL> tracker;
-    tracker.setMaxIteration(iteration);
-    tracker.setGaussianKernalSize(gaussianBlurSize);
-    tracker.setEpsilon(epsilon);
-
-    // pre-computing
-    const cv::Size kTemplateImageSize(templateSize, templateSize);
-    {
-        std::string filename = filelist[0];
-        cv::Mat image = cv::imread(filename, CV_LOAD_IMAGE_GRAYSCALE);
-
-        cv::Mat affine = MODEL::getInitial();
-        tracker.setPose(affine);
-        tracker.setTemplate(image, kTemplateImageSize);
-    }
-
     // active computing
     for(std::string& filename : filelist){
         cv::Mat image = cv::imread(filename, CV_LOAD_IMAGE_GRAYSCALE);
 
         double startTime = instant::Utils::Others::GetMilliSeconds();
-        tracker.track(image);
         double endTime = instant::Utils::Others::GetMilliSeconds();
 
-        // draw tracking result
+        // draw result
         cv::Mat color = cv::imread(filename, CV_LOAD_IMAGE_COLOR);
         if( verbose ) {
-            for(auto affineIter : tracker.getUpdatePoseList()) {
-                drawAffine(color, affineIter, kTemplateImageSize, cv::Scalar(0, 0, 255), 1);
-            }
         }
-        cv::Mat affine = tracker.getPose();
-        drawAffine(color, affine, kTemplateImageSize, cv::Scalar(0, 255, 0), 2);
+        
         cv::imshow("image", color);
         char ch = cv::waitKey(30);
         if( ch == 'q' || ch == 'Q' )
@@ -140,11 +93,8 @@ int main(int argc, char* argv[]) {
 
         if(verbose) {
             std::string message =
-                instant::Utils::String::Format("%s: iter=%03d, delta=%.3f, error=%02.3f, time=%.3fsec",
+                instant::Utils::String::Format("%s: time=%.3fsec",
                         filename.c_str(),
-                        tracker.getUpdateIteration(),
-                        tracker.getDeltaPose(),
-                        tracker.getError(),
                         (endTime-startTime)/1000.0);
             std::cout << message << std::endl;
         }
